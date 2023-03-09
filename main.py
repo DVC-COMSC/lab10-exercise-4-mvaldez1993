e1514e28d56fa5a7a3e10253c0dd89ab91b22fe6
mariav1993 <126624816+mvaldez1993@users.noreply.github.com>
2023-03-09 00:04:23 +0000

    i updated the fahrenheit and celcius

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+  celcius = int(input('Enter the Celcius Degree: '))
+  fahrenheit = (9.0/5.0) * celcius + 32 
+  print (f'The fahrenheit is {fahrenheit:.2f}')
+    
 
 
 if __name__ == '__main__':
